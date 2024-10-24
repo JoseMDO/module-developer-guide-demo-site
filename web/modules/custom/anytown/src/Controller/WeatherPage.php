fe0e7455c82dce5debff3d51c804aa51f472deb2
JoseMDO <josemontesdeocamorfin@gmail.com>
2024-10-24 17:07:53 -0500

    added extended routing to anytown wether route

--- web/modules/custom/anytown/src/Controller/WeatherPage.php
+++ web/modules/custom/anytown/src/Controller/WeatherPage.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 declare(strict_types=1);
 
@@ -14,12 +14,21 @@ class WeatherPage extends ControllerBase {
   /**
    * Builds the response.
    */
-  public function build(): array {
+  public function build(string $style): array {
+    // Style should be one of 'short', or 'extended'. And default to 'short'.
+    $style = (in_array($style, ['short', 'extended'])) ? $style : 'short';
 
     $build['content'] = [
       '#type' => 'markup',
       '#markup' => '<p>The weather forecast for this week is sunny with a chance of meatballs.</p>',
     ];
+
+    if ($style === 'extended') {
+      $build['content_extended'] = [
+        '#type' => 'markup',
+        '#markup' => '<p><strong>Extended forecast:</strong> Looking ahead to next week we expect some snow.</p>',
+      ];
+    }
 
     return $build;
   }
