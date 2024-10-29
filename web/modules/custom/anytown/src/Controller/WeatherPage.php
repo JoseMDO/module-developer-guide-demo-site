66e2a06a7e06bedd3e5ed3b6c8bb9f08becd2ec3
JoseMDO <josemontesdeocamorfin@gmail.com>
2024-10-29 10:20:24 -0500

    added render array table to weather forecast page

--- web/modules/custom/anytown/src/Controller/WeatherPage.php
+++ web/modules/custom/anytown/src/Controller/WeatherPage.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
@@ -26,7 +26,6 @@ class WeatherPage extends ControllerBase {
    * @param \Drupal\anytown\ForecastClientInterface $forecast_client
    *   Forecast API client service.
    */
-  
   public function __construct(ForecastClientInterface $forecast_client) {
     $this->forecastClient = $forecast_client;
   }
@@ -49,30 +48,73 @@ class WeatherPage extends ControllerBase {
 
     $url = 'https://raw.githubusercontent.com/DrupalizeMe/module-developer-guide-demo-site/main/backups/weather_forecast.json';
     $forecast_data = $this->forecastClient->getForecastData($url);
+
+    $rows = [];
     if ($forecast_data) {
-      $forecast = '<ul>';
+      // Create a table of the weather forecast as a render array. First loop
+      // over the forecast data and create rows for the table.
       foreach ($forecast_data as $item) {
         [
           'weekday' => $weekday,
           'description' => $description,
           'high' => $high,
           'low' => $low,
+          'icon' => $icon,
         ] = $item;
-        $forecast .= "<li>$weekday will be <em>$description</em> with a high of $high and a low of $low.</li>";
+
+        $rows[] = [
+          // Simple text for a cell can be added directly to the array.
+          $weekday,
+          // Complex data for a cell, like HTML, can be represented as a nested
+          // render array.
+          [
+            'data' => [
+              '#markup' => '<img alt="' . $description . '" src="' . $icon . '" width="200" height="200" />',
+            ],
+          ],
+          [
+            'data' => [
+              '#markup' => "<em>{$description}</em> with a high of {$high} and a low of {$low}",
+            ],
+          ],
+        ];
       }
-      $forecast .= '</ul>';
+
+      $weather_forecast = [
+        '#type' => 'table',
+        '#header' => [
+          'Day',
+          '',
+          'Forecast',
+        ],
+        '#rows' => $rows,
+        '#attributes' => [
+          'class' => ['weather_page--forecast-table'],
+        ],
+      ];
+
     }
     else {
-      $forecast = '<p>Could not get the weather forecast. Dress for anything.</p>';
+      // Or, display a message if we can't get the current forecast.
+      $weather_forecast = ['#markup' => '<p>Could not get the weather forecast. Dress for anything.</p>'];
     }
 
-    $output = "<p>Check out this weekend's weather forecast and come prepared. The market is mostly outside, and takes place rain or shine.</p>";
-    $output .= $forecast;
-    $output .= '<h3>Weather related closures</h3></h3><ul><li>Ice rink closed until winter - please stay off while we prepare it.</li><li>Parking behind Apple Lane is still closed from all the rain last week.</li></ul>';
-
-    return [
-      '#markup' => $output,
+    $build = [
+      'weather_intro' => [
+        '#markup' => "<p>Check out this weekend's weather forecast and come prepared. The market is mostly outside, and takes place rain or shine.</p>",
+      ],
+      'weather_forecast' => $weather_forecast,
+      'weather_closures' => [
+        '#theme' => 'item_list',
+        '#title' => 'Weather related closures',
+        '#items' => [
+          'Ice rink closed until winter - please stay off while we prepare it.',
+          'Parking behind Apple Lane is still closed from all the rain last weekend.',
+        ],
+      ],
     ];
+
+    return $build;
   }
 
 }
